87b90235872d368702317c26c81b10a9621b4c3f
Kristin Do <kristindo007555@gmail.com>
2025-07-09 01:29:40 +1000

    Input fields for contact form

--- frontend/src/Modal/FormModal.js
+++ frontend/src/Modal/FormModal.js
@@ -2,9 +2,7 @@ import { X } from 'lucide-react';
 import { useState } from 'react';
 import styles from './FormModal.module.css';
 
-
 function FormModal({ onClose }) {
-
   const [isClosing, setIsClosing] = useState(false);
 
   const handleClose = () => {
@@ -13,21 +11,49 @@ function FormModal({ onClose }) {
       onClose();
       setIsClosing(false);
     }, 300); // Match this duration with the CSS animation duration
-  }
+  };
   return (
     <div className={`${styles.container} ${isClosing ? styles.closing : ''}`}>
-     <div
-        className={`${styles.modalcontainer} ${isClosing ? styles.closing : ''}`}
+      <div
+        className={`${styles.modalcontainer} ${
+          isClosing ? styles.closing : ''
+        }`}
       >
         <div>
           <button className={styles.close} onClick={handleClose}>
             <X />
           </button>
           <div>
-            <h3 className = {styles.title}>contact me !</h3>
-            <form>
-              <input type="email" placeholder="enter your email" required />
-              <input type="text" placeholder="enter your message" required />
+            <h3 className={styles.title}>contact me !</h3>
+
+            <form className = {styles.form}>
+              <div >
+                <label className={styles.email} htmlFor="email">
+                  email:
+                </label>
+                <input
+                className = {styles.email_input}
+                  name="email"
+                  id="email"
+                  type="email"
+                  placeholder="enter your email"
+                  required
+                />
+              </div>
+              <div>
+                <label className={styles.message} htmlFor="message">
+                  message:
+                </label>
+                <textarea
+                className = {styles.message_input}
+                  name="message"
+                  id="message"
+                  type="text"
+                  placeholder="enter your message"
+
+                  required
+                />
+              </div>
               <button type="submit">send</button>
             </form>
           </div>
