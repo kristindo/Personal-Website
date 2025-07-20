06e37ecbf2dff79131c4caff7bcd0628276a5294
Kristin Do <kristindo007555@gmail.com>
2025-07-21 01:03:48 +1000

    fixed github link

--- frontend/src/Modal/FormModal.js
+++ frontend/src/Modal/FormModal.js
@@ -100,7 +100,7 @@ function FormModal({ onClose }) {
         </a>
         <a
           rel="noopener noreferrer"
-          href="https://www.github.com/kristin-do/"
+          href="https://www.github.com/kristindo/"
           target="_blank"
         >
           <img
