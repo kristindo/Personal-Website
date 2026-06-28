70483429d7d7f17d24b24231a287331d8f0f03a2
Kristin Do <kristindo007555@gmail.com>
2026-06-28 21:52:38 +1000

    new job

--- frontend/src/components/Experience/Experience.js
+++ frontend/src/components/Experience/Experience.js
@@ -6,6 +6,18 @@ function Experience() {
       <div>
         <div className={styles.job}>
           <div className={styles.company}>
+            <p className = {styles.name}>EY Graduate - </p>{' '}
+            <p className={styles.role}> hrt consultant </p>
+          </div>
+          <div className={styles.date}>mar 2026</div>
+        </div>
+        <div className={styles.tools_container}>
+          {' '}
+          hr technologies 
+        </div>
+      </div>
+      <div className={styles.job}>
+          <div className={styles.company}>
             <p className = {styles.name}>Interactive Student Satisfaction Survey - </p>{' '}
             <p className={styles.role}> software developer </p>
           </div>
@@ -15,7 +27,6 @@ function Experience() {
           {' '}
           cakephp, mysql, svg, css, html
         </div>
-      </div>
     </div>
   );
 }
